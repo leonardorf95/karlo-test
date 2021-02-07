18f5eb270bb0df18180d047564bc0535c3c30516
Leonardo <luisfonseca9521@gmail.com>
2021-02-07 10:56:16 -0600

    Create files for routes and app files to config server

---
+++ API/src/app.js
@@ -0,0 +1,26 @@
+// Importación de paquetes Node
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// Importacion de las rutas
+import authRouter from './routes/authRoute.js';
+import productRouter from './routes/productRoute.js';
+import purchaseOrderRouter from './routes/purchaseOrderRoute.js';
+
+// Inicializacion de server
+const server = express();
+
+// Implementación de recepción de formantos Json
+server.use(bodyParser.json());
+
+// Implementación de cors (permitir accesos)
+server.use(cors());
+
+// Implementación de rutas de la solución
+server.use('/api', authRouter);
+server.use('/api', productRouter);
+server.use('/api', purchaseOrderRouter);
+
+// Exportación de toda la configuración del servidor
+export default server;
