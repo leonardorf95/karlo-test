f7e27846212ecdd1356a0b6cabec083997237370
Leonardo <luisfonseca9521@gmail.com>
2021-02-08 21:38:13 -0600

    Fixed import

--- API/src/routes/purchaseOrderRoute.js
+++ API/src/routes/purchaseOrderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import PurchaseOrderController from '../controllers/purchaseOrderController';
+import PurchaseOrderController from '../controllers/purchaseOrderController.js';
 
 const router = express.Router();
 
