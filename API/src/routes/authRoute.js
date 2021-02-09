e1479b52f5f203a740986b60c6a7a9bdf692ee15
Leonardo <luisfonseca9521@gmail.com>
2021-02-08 23:04:35 -0600

    Add routes for auth

--- API/src/routes/authRoute.js
+++ API/src/routes/authRoute.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import AuthController from '../controllers/authController.js';
 
 const router = express.Router();
+
+router.post('/auth', AuthController.singUp);
+router.post('/auth/:email', AuthController.validateAccount);
+router.post('/auth', AuthController.singIn);
 
 export default router;
