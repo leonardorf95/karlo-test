12e525bf62d34d4aeec237785c834fcbbad19ce7
Leonardo <luisfonseca9521@gmail.com>
2021-02-08 21:07:50 -0600

    Add routing for products

--- API/src/routes/productRoute.js
+++ API/src/routes/productRoute.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import ProductController from '../controllers/productController.js';
 
 const router = express.Router();
+
+router.get('/products', ProductController.getAlls);
+router.get('/products/:id', ProductController.getById);
+router.post('/products', ProductController.create);
+router.put('/products/:id', ProductController.updated);
+router.delete('/products/:id', ProductController.delete);
 
 export default router;
