26837ef9c858753a25e8926c5925c75d8c747c53
Leonardo <luisfonseca9521@gmail.com>
2021-02-08 21:37:40 -0600

    Add routing file for purchase

--- API/src/routes/purchaseOrderRoute.js
+++ API/src/routes/purchaseOrderRoute.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import PurchaseOrderController from '../controllers/purchaseOrderController';
 
 const router = express.Router();
+
+router.get('/purchase', PurchaseOrderController.getAlls);
+router.get('/purchase/:id', PurchaseOrderController.getById);
+router.post('/purchase', PurchaseOrderController.create);
+router.put('/purchase/:id', PurchaseOrderController.updated);
+router.delete('/purchase/:id', PurchaseOrderController.delete);
 
 export default router;
