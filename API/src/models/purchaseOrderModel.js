07d0e82fc5321288d0bcc4c790cd460a362bd68d
Leonardo <luisfonseca9521@gmail.com>
2021-02-08 22:46:47 -0600

    Fix on model

--- API/src/models/purchaseOrderModel.js
+++ API/src/models/purchaseOrderModel.js
@@ -1,9 +1,7 @@
 import sequelize from 'sequelize';
 import dbConfig from '../database/dbConfig.js';
 
-import ProductModel from './productModel.js';
-
-const PurchaseOrderModel = dbConfig.define('product', {
+const PurchaseOrderModel = dbConfig.define('purchase', {
     id: {
         type: sequelize.INTEGER,
         primaryKey: true,
@@ -14,7 +12,7 @@ const PurchaseOrderModel = dbConfig.define('product', {
         allowNull: false
     },
     iva: {
-        type: sequelize.INTEGER(11),
+        type: sequelize.DECIMAL(18, 2),
         allowNull: false
     },
     total: {
@@ -23,7 +21,5 @@ const PurchaseOrderModel = dbConfig.define('product', {
     },
     createdAt: sequelize.DATE
 });
-
-PurchaseOrderModel.hasMany(ProductModel);
 
 export default PurchaseOrderModel;
