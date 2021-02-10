a9cf272438eb8c7231ea1f1c3ab7e0d693f19ac1
Leonardo <luisfonseca9521@gmail.com>
2021-02-10 06:21:19 -0600

    Protected routes

--- API/src/routes/productRoute.js
+++ API/src/routes/productRoute.js
@@ -1,12 +1,34 @@
 import express from 'express';
+
 import ProductController from '../controllers/productController.js';
+import AuthMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/products', ProductController.getAlls);
-router.get('/products/:id', ProductController.getById);
-router.post('/products', ProductController.create);
-router.put('/products/:id', ProductController.updated);
-router.delete('/products/:id', ProductController.delete);
+router.get('/products', 
+    AuthMiddleware.verifyToken,
+    ProductController.getAlls
+);
+
+router.get('/products/:id', 
+    AuthMiddleware.verifyToken,
+    ProductController.getById
+);
+
+router.post('/products', 
+    AuthMiddleware.verifyToken,
+    ProductController.create
+);
+
+router.put('/products/:id', 
+    AuthMiddleware.verifyToken,
+    ProductController.updated
+);
+
+router.delete('/products/:id', 
+    AuthMiddleware.verifyToken,
+    ProductController.delete
+);
+
 
 export default router;
--- API/src/routes/purchaseOrderRoute.js
+++ API/src/routes/purchaseOrderRoute.js
@@ -1,12 +1,34 @@
 import express from 'express';
+
 import PurchaseOrderController from '../controllers/purchaseOrderController.js';
+import AuthMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/purchase', PurchaseOrderController.getAlls);
-router.get('/purchase/:id', PurchaseOrderController.getById);
-router.post('/purchase', PurchaseOrderController.create);
-router.put('/purchase/:id', PurchaseOrderController.updated);
-router.delete('/purchase/:id', PurchaseOrderController.delete);
+router.get('/purchase',
+    AuthMiddleware.verifyToken,
+    PurchaseOrderController.getAlls
+);
+
+router.get('/purchase/:id',
+    AuthMiddleware.verifyToken,
+    PurchaseOrderController.getById
+);
+
+router.post('/purchase',
+    AuthMiddleware.verifyToken,
+    PurchaseOrderController.create
+);
+
+router.put('/purchase/:id',
+    AuthMiddleware.verifyToken,
+    PurchaseOrderController.updated
+);
+
+router.delete('/purchase/:id',
+    AuthMiddleware.verifyToken,
+    PurchaseOrderController.delete
+);
+
 
 export default router;
