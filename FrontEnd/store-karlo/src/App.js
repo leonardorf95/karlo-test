b9466f172e2410b92309de0ce0efa4a1ed087a71
Leonardo <luisfonseca9521@gmail.com>
2021-02-11 21:42:28 -0600

    Define routes and implement provide store

--- FrontEnd/store-karlo/src/App.js
+++ FrontEnd/store-karlo/src/App.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 import Main from './components/layout/Main';
 
 import SingUp from './components/auth/SingUp';
@@ -8,7 +11,6 @@ import SingIn from './components/auth/SingIn';
 import ForgotPassword from './components/auth/ForgotPassword';
 
 import Products from './components/produts/Products';
-import Product from './components/produts/Product';
 import FormNewProduct from './components/produts/FormNewProduct';
 import FormEditProduct from './components/produts/FormEditProduct';
 
@@ -17,31 +19,33 @@ import Purchase from './components/purchase/Purchase';
 import FormNewPurchase from './components/purchase/FormNewPurchase';
 
 import Users from './components/users/Users';
-import User from './components/users/User';
 import FormNewUser from './components/users/FormNewUser';
 import FormEditUser from './components/users/FormEditUser';
 
 const App = () => {
-  return(
+  return (
     <Fragment>
       <Router>
-        <Switch>
-          <Route exact path='/' component={Main} />
-          <Route exact path='/auth/singup' component={SingUp} />
-          <Route exact path='/auth/singin' component={SingIn} />
-          <Route exact path='/auth/forgot' component={ForgotPassword} />
-          <Route exact path='/products/' component={Products} />
-          <Route exact path='/products/:id' component={Product} />
-          <Route exact path='/products/new' component={FormNewProduct} />
-          <Route exact path='/products/edit/:id' component={FormEditProduct} />
-          <Route exact path='/purchase/' component={Purchases} />
-          <Route exact path='/purchase/new' component={FormNewPurchase} />
-          <Route exact path='/purchase/:id' component={Purchase} />
-          <Route exact path='/users/' component={Users} />
-          <Route exact path='/users/:id' component={User} />
-          <Route exact path='/users/new' component={FormNewUser} />
-          <Route exact path='/users/edit/:id' component={FormEditUser} />
-        </Switch>
+        <Provider store={store}>
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/auth/singup' component={SingUp} />
+            <Route exact path='/auth/singin' component={SingIn} />
+            <Route exact path='/auth/forgot' component={ForgotPassword} />
+
+            <Route exact path='/products/' component={Products} />
+            <Route exact path='/products/new' component={FormNewProduct} />
+            <Route exact path='/products/edit/:id' component={FormEditProduct} />
+
+            <Route exact path='/purchase/' component={Purchases} />
+            <Route exact path='/purchase/new' component={FormNewPurchase} />
+            <Route exact path='/purchase/:id' component={Purchase} />
+
+            <Route exact path='/users/' component={Users} />
+            <Route exact path='/users/new' component={FormNewUser} />
+            <Route exact path='/users/edit/:id' component={FormEditUser} />
+          </Switch>
+        </Provider>
       </Router>
     </Fragment>
   )
