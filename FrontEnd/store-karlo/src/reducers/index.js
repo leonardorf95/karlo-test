862c83f0d8bb93396177248936292780adfee726
Leonardo <luisfonseca9521@gmail.com>
2021-02-11 23:20:18 -0600

    Add all reducer for to use in the components

--- FrontEnd/store-karlo/src/reducers/index.js
+++ FrontEnd/store-karlo/src/reducers/index.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import productReducer from './productsReducer';
 import alertReducer from './alertReducer';
+import userReducer from './userReducer';
+import purchaseReducer from './purchaseReducer';
 
 export default combineReducers({
     products: productReducer,
-    alert: alertReducer
+    alert: alertReducer,
+    users: userReducer,
+    purchase: purchaseReducer
 });
