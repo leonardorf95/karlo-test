0a2a804d2618bff032926db13177655092d67762
Leonardo <luisfonseca9521@gmail.com>
2021-02-11 23:19:39 -0600

    change order menu

--- FrontEnd/store-karlo/src/components/layout/Aside.js
+++ FrontEnd/store-karlo/src/components/layout/Aside.js
@@ -9,7 +9,7 @@ const Aside = () => {
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark align-items-start flex-sm-column flex-row">
                     &nbsp;
                     <Link to='/' className='navbar-brand'>
-                    <i className="bi bi-house"></i>Karlo Store
+                        <i className="bi bi-house"></i>Karlo Store
                     </Link>
 
                     <a href className="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
@@ -18,6 +18,12 @@ const Aside = () => {
 
                     <div className="collapse navbar-collapse sidebar">
                         <ul className="flex-column navbar-nav w-100 justify-content-between">
+                            <li className="nav-item">
+                                <Link to='/users/' className='nav-link px-0 text-truncate'>
+                                    <i className="fa fa-bullseye fa-fw"></i>Usuarios
+                                </Link>
+                            </li>
+
                             <li className="nav-item">
                                 <Link to='/products/' className='nav-link px-0 text-truncate'>
                                     <i className="fa fa-bullseye fa-fw"></i>Productos
@@ -29,13 +35,7 @@ const Aside = () => {
                                     <i className="fa fa-bullseye fa-fw"></i> Ordenes de Compra
                                 </Link>
                             </li>
-
-                            <li className="nav-item">
-                                <Link to='/users/' className='nav-link px-0 text-truncate'>
-                                    <i className="fa fa-bullseye fa-fw"></i>Usuarios
-                                </Link>
-                            </li>
-
+                            
                             <li className="nav-item">
                                 <Link to='/' className='nav-link px-0 text-truncate'>
                                     <i className="fa fa-bullseye fa-fw"></i> Cerrar Sesión
