966d28b2914a3527af7ea1f70d6dc199c8492320
Leonardo <luisfonseca9521@gmail.com>
2021-02-11 21:40:05 -0600

    Remove Components

--- FrontEnd/store-karlo/src/components/produts/Product.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Product = () => {
-    return(
-        <h1>Detalle del Produto</h1>
-    )
-}
-
-export default Product;
--- FrontEnd/store-karlo/src/components/users/User.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const User = () => {
-    return(
-        <h1>Detalle del usuarios</h1>
-    )
-}
-
-export default User;
