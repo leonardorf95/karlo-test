3a97469b2e1e15dda3e32025067acba7b7fff870
Leonardo <luisfonseca9521@gmail.com>
2021-02-12 00:33:44 -0600

    import the auth reducer with others

--- FrontEnd/store-karlo/src/reducers/index.js
+++ FrontEnd/store-karlo/src/reducers/index.js
@@ -4,10 +4,12 @@ import productReducer from './productsReducer';
 import alertReducer from './alertReducer';
 import userReducer from './userReducer';
 import purchaseReducer from './purchaseReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
     products: productReducer,
     alert: alertReducer,
     users: userReducer,
-    purchase: purchaseReducer
+    purchase: purchaseReducer,
+    auth: authReducer
 });
