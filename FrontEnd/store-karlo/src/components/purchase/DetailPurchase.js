9b53333e487f15d27d9a72b8de6d99d63a6c107b
Leonardo <luisfonseca9521@gmail.com>
2021-02-12 13:36:45 -0600

    Create component for show detail purchase

---
+++ FrontEnd/store-karlo/src/components/purchase/DetailPurchase.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const DetailPurchase = () =>{
+    return(
+        <Fragment>
+            Hola
+        </Fragment>
+    )
+}
+
+export default DetailPurchase;
