27f9fb7b433a14be50682ad9a54658b8ff0bde47
Leonardo <luisfonseca9521@gmail.com>
2021-02-12 21:08:48 -0600

    Add font awesome

--- FrontEnd/store-karlo/src/components/layout/Aside.js
+++ FrontEnd/store-karlo/src/components/layout/Aside.js
@@ -21,7 +21,7 @@ const Aside = () => {
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark align-items-start flex-sm-column flex-row">
                     &nbsp;
                     <Link to='/' className='navbar-brand'>
-                        <i className="bi bi-house"></i>Karlo Store
+                        <i class="fas fa-store"></i> Karlo Store
                     </Link>
 
                     <a href className="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
@@ -32,29 +32,29 @@ const Aside = () => {
                         <ul className="flex-column navbar-nav w-100 justify-content-between">
                             <li className="nav-item">
                                 <Link to='/users/' className='nav-link px-0 text-truncate'>
-                                    <i className="fa fa-bullseye fa-fw"></i>Usuarios
+                                    <i class="fas fa-users"></i>Usuarios
                                 </Link>
                             </li>
 
                             <li className="nav-item">
                                 <Link to='/products/' className='nav-link px-0 text-truncate'>
-                                    <i className="fa fa-bullseye fa-fw"></i>Productos
+                                    <i class="fas fa-tshirt"></i>Productos
                                 </Link>
                             </li>
 
                             <li className="nav-item">
                                 <Link to='/purchase/' className='nav-link px-0 text-truncate'>
-                                    <i className="fa fa-bullseye fa-fw"></i> Ordenes de Compra
+                                    <i class="fas fa-file-invoice"></i> Ordenes de Compra
                                 </Link>
                             </li>
-                            
+
                             <li className="nav-item">
-                                <button 
-                                    type='button' 
-                                    className='btn btn-link px-0 text-truncate' 
+                                <button
+                                    type='button'
+                                    className='btn btn-link px-0 text-truncate'
                                     onClick={submitLogut}
                                 >
-                                    <i className="fa fa-bullseye fa-fw"></i> Cerrar Sesión
+                                    <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
                                 </button>
                             </li>
                         </ul>
